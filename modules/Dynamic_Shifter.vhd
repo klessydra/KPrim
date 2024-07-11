f04d5057c51a966d634ef2f3ef372e32b7f6db53
Abdallah Cheikh <abdallah.cheikh@uniroma1.it>
2024-07-11 18:32:00 +0200

    made the dynamic shifter generic

--- modules/Dynamic_Shifter.vhd
+++ modules/Dynamic_Shifter.vhd
@@ -18,6 +18,16 @@
 -- 
 ----------------------------------------------------------------------------------
 
+----------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
+--  ██████╗░██╗░░░██╗███╗░░██╗░█████╗░███╗░░░███╗██╗░█████╗░      ░██████╗██╗░░██╗██╗███████╗████████╗███████╗██████╗░
+--  ██╔══██╗╚██╗░██╔╝████╗░██║██╔══██╗████╗░████║██║██╔══██╗      ██╔════╝██║░░██║██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗
+--  ██║░░██║░╚████╔╝░██╔██╗██║███████║██╔████╔██║██║██║░░╚═╝      ╚█████╗░███████║██║█████╗░░░░░██║░░░█████╗░░██████╔╝
+--  ██║░░██║░░╚██╔╝░░██║╚████║██╔══██║██║╚██╔╝██║██║██║░░██╗      ░╚═══██╗██╔══██║██║██╔══╝░░░░░██║░░░██╔══╝░░██╔══██╗
+--  ██████╔╝░░░██║░░░██║░╚███║██║░░██║██║░╚═╝░██║██║╚█████╔╝      ██████╔╝██║░░██║██║██║░░░░░░░░██║░░░███████╗██║░░██║
+--  ╚═════╝░░░░╚═╝░░░╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░╚════╝░      ╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝
+----------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -25,39 +35,47 @@ use IEEE.NUMERIC_STD.ALL;
 
 
 entity Dynamic_Shifter is
-    Port ( input : in STD_LOGIC_VECTOR (63 downto 0);
-           shift_amt : in unsigned(4 downto 0);
-           output : out STD_LOGIC_VECTOR (63 downto 0));
+  generic (
+    size       : natural;
+    size_width : natural
+  );
+  Port (
+    input          : in  STD_LOGIC_VECTOR ((size*2)-1 downto 0);
+    shift_amt      : in  unsigned(size_width-1 downto 0);
+    shifter_enable : in  STD_LOGIC;
+    output         : out STD_LOGIC_VECTOR ((size*2)-1 downto 0)
+  );
 end Dynamic_Shifter;
 
 architecture Behavioral of Dynamic_Shifter is
-constant allzeros       : std_logic_vector(32 downto 0) := (others=>'0');
+
+  constant allzeros : std_logic_vector(size downto 0) := (others => '0');
 begin
-shifter:process(shift_amt,input)
-variable temp : std_logic_vector(63 downto 0);
-begin
+  shifter : process(shift_amt,input)
+    variable temp : std_logic_vector((size*2)-1 downto 0);
+  begin
     -- The shifter is realized as the cascade of three MUX. This reduce area consumption
     temp := input;
-    case shift_amt(4 downto 4) is
-        when "0"    =>  temp := temp; 
-        when others =>  temp := temp (47 downto 0) & allzeros(15 downto 0);
-    end case;
-    
-    case shift_amt( 3 downto 2) is
-        when "00"   =>  temp := temp; 
-        when "01"   =>  temp := temp (59 downto 0) & allzeros(3 downto 0);
-        when "10"   =>  temp := temp (55 downto 0) & allzeros(7 downto 0);
-        when others =>  temp := temp (51 downto 0) & allzeros(11 downto 0);
-    end case;
-    
-    case shift_amt( 1 downto 0) is
-        when "00"   =>  temp := temp; 
-        when "01"   =>  temp := temp (62 downto 0) & allzeros(0 downto 0);
-        when "10"   =>  temp := temp (61 downto 0) & allzeros(1 downto 0);
-        when others =>  temp := temp (60 downto 0) & allzeros(2 downto 0);
-    end case;   
+    if (shifter_enable = '0') then
+      temp := temp;
 
-    output<=temp;
-end process;
+      if size_width mod 2 = 1 then
+        if shift_amt(size_width-1 downto size_width-1) = "1" then
+          temp := temp((size*2)-(2**(size_width-1))-1 downto 0) & allzeros((2**(size_width-1))-1 downto 0);
+        end if;
+      end if;
+
+      for i in 0 to (size_width/2)-1 loop -- size_width/2 was used in this loop because we are handling two bits of shift_amt at a time instead of '1'
+        for j in 1 to 3 loop -- index 1 to 3 
+          if shift_amt(i*2+1 downto i*2) = j then
+            temp := temp ((size*2)-j*(4**i)-1 downto 0) & allzeros(j*(4**i)-1 downto 0);
+          end if;
+        end loop;
+      end loop;
+
+    end if;
+    output <= temp;
+  end process;
 
 end Behavioral;
+
