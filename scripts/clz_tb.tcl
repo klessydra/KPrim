adb46e4a9a7236437a10570cea5a7c7d3d259aaa
Abdallah Cheikh <abdallah.cheikh@uniroma1.it>
2024-08-14 10:18:51 +0200

    .

--- scripts/clz_tb.tcl
+++ scripts/clz_tb.tcl
@@ -2,7 +2,7 @@
 mkdir -p prim_libs
 vlib prim_libs/clz_lib
 vcom  tb/clz_tb.vhd -work prim_libs/clz_lib
-vcom -2008 modules/Binary\ Counters/clz_for.vhd   		-work prim_libs/clz_lib
-vcom -2008 modules/Binary\ Counters/clz_decomposing.vhd   -work prim_libs/clz_lib
+vcom -2008 modules/Binary\ Counters/clz_for.vhd         -work prim_libs/clz_lib
+vcom -2008 modules/Binary\ Counters/clz_decomposing.vhd -work prim_libs/clz_lib
 vsim -voptargs=+acc prim_libs/clz_lib.clz_tb
 add wave -r sim:/clz_tb/*
